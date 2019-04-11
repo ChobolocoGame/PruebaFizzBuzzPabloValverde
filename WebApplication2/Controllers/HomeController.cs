c5fe53e08260ea3e91c4c8b4d3735f339eb674e3
Vlichy-PC\Vlichy <paniovalsal@gmail.com>
2019-04-11 22:32:49 +0200

    Prueba

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using FizzBuzz.Services;
+using WebApplication2.ViewModels.Home;
+
+namespace FizzBuzz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFizzBuzzService _fizzBuzzService;
+
+        public HomeController()
+        {
+            _fizzBuzzService = new FizzBuzzService();
+        }
+
+        public HomeController(IFizzBuzzService fizzBuzzService)
+        {
+            _fizzBuzzService = fizzBuzzService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*Enviamos el formulario vía POST*/
+        [HttpPost]
+        public ActionResult Index(IndexViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Result = null;
+                return View(viewModel);
+            };
+
+            viewModel.Result = _fizzBuzzService.Check(viewModel.Number.Value);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebApplication2/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/*Declaramos campo requerido y Number nullable*/
+namespace WebApplication2.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        [Required, Range(1, 100)]
+        public int? Number { get; set; }
+        public string Result { get; set; }
+    }
+}
