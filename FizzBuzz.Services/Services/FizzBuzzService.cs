2000a01055f79757958d9813e14bad8899e2ef86
Vlichy-PC\Vlichy <paniovalsal@gmail.com>
2019-04-13 13:44:02 +0200

    Subida de la carpeta Services y test

---
+++ FizzBuzz.Services/Services/FizzBuzzService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+/*Creamos la lógica del juego FizzBuzz donde capturamos el rango del valor*/
+namespace FizzBuzz.Services
+{ 
+    public class FizzBuzzService : IFizzBuzzService
+    {
+        public string Check(int number)
+        {
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException(string.Format("valores comprendido entre uno y cien", number));
+            }
+            if (number % 15 == 0) return "FizzBuzz";
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+            return number.ToString();
+        }
+    }
+}
---
+++ FizzBuzz.Tests/Controller/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿
+using NUnit.Framework;
+using FizzBuzz.Services;
+namespace FizzBuzz.Web.Tests.Controller
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private class DummyFizzBuzzService : IFizzBuzzService
+        {
+            public string Check(int number) { return "FizzBuzzDummyResult"; }
+        }
+
+        [Test]
+        public void CanCreate()
+        {
+            var controller = new HomeControllerTests();
+            Assert.IsNotNull(controller);
+        }
+
+       
+
+        //[Test]
+        //public void CheckReturnsCorrectView()
+        //{
+        //    var service = new DummyFizzBuzzService();
+        //    var controller = new Controller(service);
+
+        //    var result = (ViewResult)controller.Index();
+
+        //    Assert.AreEqual("Index", result.ViewName);
+        //}
+
+        
+    }
+}
